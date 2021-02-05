4622bc97dcff2664b2558c0982d01c1e58843ee8
muhammadlovlu <chyshahab@gmail.com>
2021-02-05 14:26:01 +0600

    updated

--- template.js
+++ template.js
@@ -7,6 +7,20 @@ console.log(fullName);
 ///////////
 const firstname = "Richard";
 const lastname ="Mond";
-const fullname = `${firstname} ${lastname}` ;
+const fullname = `${firstname} ${lastname} is an athlete` ;
 console.log(fullname);
+
+/////
+const FirstName = 'Cathrine';
+const LastName = 'Sally';
+const FullName = `${FirstName} ${LastName} How may i help You`;
+console.log(FullName);
+
+
+const FIRSTNAME = 'David';
+const LASTNAME = 'Jourge';
+const FULLNAME = `${FIRSTNAME} ${LASTNAME} How are you`;
+console.log(FULLNAME);
+
+
 
