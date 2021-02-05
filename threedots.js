047a645bfc11ac480e27d8164c19ca4c4d7edb70
muhammadlovlu <chyshahab@gmail.com>
2021-02-05 18:40:24 +0600

    updated

--- threedots.js
+++ threedots.js
@@ -4,9 +4,11 @@ const areaTwo = [45,46,47,48,49,50];
 console.log('First Area :', areaOne);
 console.log('Second Area :', areaTwo);
 
-/////////////////////////////
+//applied ES6 method in a variable to see the output 
 const areaone = [22,33,44,55,66,77,88,99];
 const areatwo = [122,133,144,155,166,177,188, 199];
-const totalArea = areaone.concat(areatwo);
+const totalArea = `Area Total : ${areaone.concat(areatwo)}`;
 console.log(totalArea);
+
+////////////////////
 
