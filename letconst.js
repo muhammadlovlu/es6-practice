b0859a77a880a323970becb342fc7f02698f1f7a
muhammadlovlu <chyshahab@gmail.com>
2021-02-05 11:28:48 +0600

    updated

---
+++ letconst.js
@@ -0,0 +1,20 @@
+const gender = "male";
+// gender = "female";
+
+console.log(gender);
+
+const digits = [12,45, 75,22, 36,19,25];
+digits [0] = 33; // repalaced 12 digits
+digits [1] = 90; // repalaced 45 digits
+digits [2] = 65; // repalaced 75 digits
+digits [3] = 42; // repalaced 22 digits
+digits [4] = 55; // repalaced 36 digits
+digits [5] = 100; // repalaced 19 digits
+digits [6] = 200; // repalaced 25 digits
+
+
+
+// try to  
+digits.push = 250, 99;
+// digits.push(260);
+console.log(digits);
