3f265509cb486b32129abc5e4969236517dad203
muhammadlovlu <chyshahab@gmail.com>
2021-02-05 17:42:34 +0600

    updated

---
+++ threedots.js
@@ -0,0 +1,8 @@
+// Simple example 
+const areaOne = [15,16,17,18,19,20];
+const areaTwo = [45,46,47,48,49,50];
+console.log('First Area :', areaOne);
+console.log('Second Area :', areaTwo);
+
+/////////////////////////////
+const 
