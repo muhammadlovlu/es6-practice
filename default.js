bf4843f7092f4a723392b457f133537303e1430b
muhammadlovlu <chyshahab@gmail.com>
2021-02-05 12:05:55 +0600

    updated

--- default.js
+++ default.js
@@ -1,5 +1,16 @@
-function add(number1, number2){
- return number1 + number2;
+function add(number1, number2 = 0){
+    return number1 + number2;
 }
-const total = add(20, 20);
+const total = add(30);
 console.log(total);
+
+
+///////////////////////
+function addTwo (number3 , number4){
+    number4 = number4 || 0;
+    return number3 + number4;
+}
+const secondTotal = addTwo (12);
+console.log(secondTotal);
+
+//////////////////////////
