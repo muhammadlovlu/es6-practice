a0fc002693a53cc0d9654abea435e8e8871a2946
muhammadlovlu <chyshahab@gmail.com>
2021-02-05 15:47:45 +0600

    updated

--- template.js
+++ template.js
@@ -4,17 +4,20 @@ const lastName = "Bush";
 const fullName = firstName + " " + lastName +" is an animal Lover.";
 console.log(fullName);
 
+
 ///////////
 const firstname = "Richard";
 const lastname ="Mond";
 const fullname = `${firstname} ${lastname} is an athlete` ;
 console.log(fullname);
 
+
 /////
 const FirstName = 'Cathrine';
 const LastName = 'Sally';
 const FullName = `${FirstName} ${LastName} How may i help You`;
 console.log(FullName);
+
 
 // Variable name should be camel case if there is word more than one . Its just a practice purpose i used capital letter .
 const FIRSTNAME = 'David';
@@ -30,14 +33,12 @@ const allLine = `${firstLine} ${secondLine}`;
 console.log(allLine);
 
 
-
 // Show line wise text 
 const firstline = "hello My Friend";
 const secondline = "How may i help you";
 const allline = `${firstline} 
 ${secondline}`;
 console.log(allline);
-
 
 
 /////////////////////////////////
