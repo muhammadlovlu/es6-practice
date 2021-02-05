8572687bb7a68b58531c25a130a1103f643f448e
muhammadlovlu <chyshahab@gmail.com>
2021-02-05 12:24:50 +0600

    updated

---
+++ template.js
@@ -0,0 +1,12 @@
+///////////////
+const firstName = "Goerge";
+const lastName = "Bush";
+const fullName = firstName + " " + lastName +" is an animal Lover.";
+console.log(fullName);
+
+///////////
+const firstname = "Richard";
+const lastname ="Mond";
+const fullname = `${firstname} ${lastname}` ;
+console.log(fullname);
+
