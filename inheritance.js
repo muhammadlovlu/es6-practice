dae6fc14204a23d5ee86dfc9f907cc7f4898ee5e
muhammadlovlu <chyshahab@gmail.com>
2021-02-06 21:37:11 +0600

    updated

--- inheritance.js
+++ inheritance.js
@@ -1,3 +1,4 @@
+// Parent and child example
 class Parent {
     constructor(){
         this.fatherName = "Ramond";
@@ -13,7 +14,9 @@ class Child extends Parent{
 const baby = new Child("Arnold");
 const baby2 = new Child
 console.log(baby);
-/////////
+///////// Shop inventory example
+
+
 class Shop {
     constructor (){
         this.shopName = 'MLC';
@@ -39,3 +42,29 @@ console.log(product01);
 console.log(product02);
 console.log(product03);
 console.log(product04)
+
+
+// inheritance example three
+class Country {
+    constructor(){
+        this.country = 'UK';
+    }
+}
+class Keyboard extends Country{
+    constructor(Brand, SN, Warranty,Color, KeyPress, Mfg){
+        super ();
+        this.brand = Brand;
+        this.snNumber = SN;
+        this.warranty = Warranty;
+        this.color = Color;
+        this.keypress = KeyPress;
+        this.mfgDate = Mfg;
+
+    }
+}
+const Keyboard1000 = new Keyboard('Logitech', '11454545889', '2Years', 'Black', '24000', '127.2019');
+const keyboard1010 = new Keyboard('Asus', '4577585466', '1Years', 'Blue', '1000', '12.12.2018');
+const keyboard1020 = new Keyboard('Dell', '362254645', '3Years', 'Red', '4500', '20.3.2017');
+console.log(Keyboard1000);
+console.log(keyboard1010);
+console.log(keyboard1020);
